d27e4ce49d60ff3a5b90962301363b1e4c0d2b7d
Aytuhan Kazandirmak <aytuhann@gmail.com>
2022-11-18 23:46:54 +0300

    first javascript

---
+++ main.js
@@ -0,0 +1,11 @@
+let isim = prompt("Lütfen adınızı giriniz.")
+document.getElementById("info").innerHTML = isim;
+
+const clock = new Date();
+document.getElementById("clock").innerHTML = clock.toDateString("tr-TR")
+
+ 
+
+
+
+
